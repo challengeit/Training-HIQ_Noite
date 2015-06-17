2bceb656e820354bc78b92914d17aacb53ce66ac
Ricardo Sousa <ricardo.sousa@challenge.pt>
2015-06-17 18:04:49 +0100

    organize folders.

---
+++ Sessao_5/src/app/App.java
@@ -0,0 +1,58 @@
+package app;
+
+public class App 
+{
+	public static void main(String[] args) 
+	{
+//		int number = 10;
+//		
+//		long longNumber = 10;
+		
+		
+//		System.out.println(Short.MAX_VALUE);
+//		short shortNumber = (short)1000000;
+//		System.out.println(shortNumber);
+		
+//		int num1 = Integer.MAX_VALUE;
+//		System.out.println(num1 + 1);
+//		System.out.println(Integer.MIN_VALUE);
+		
+//		final byte b1 = 1;
+//		final byte b2 = 1;
+//		byte b = b1 + b2;
+//		System.out.println(b);
+//		System.out.println(Byte.MIN_VALUE);
+		
+//		double d = 1343223423424234234242342342423423.2;
+//		System.out.println(d);
+		
+//		float f = 1343223423424234234242342342423423.2f;
+//		System.out.println(f);
+		
+//		double d1 = 1/2.0;
+//		System.out.println(d1);
+		
+//		char c = 'a';
+//		System.out.println(c);
+//		int i = c;
+//		System.out.println(i);
+		
+//		final int b1 = 1;
+//		byte b = b1;
+//		
+//		byte b4 = 10;
+//		
+//		final int b2 = 1;
+//		byte b3 = b2;
+//
+//		final long l = 10;
+//		int l1 = (int)l;
+		String a = "lala";
+		String s = "lala";
+		
+		int ia = 42;
+		Integer i = new Integer(2);
+		Integer j = 42;
+		long l = 42;
+	}
+}
